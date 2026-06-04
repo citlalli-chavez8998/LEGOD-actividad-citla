b8b3714347305d30eb29f63fae85c76f179540eb
zuzu-8998 <chavez.nz.citlalli@gmail.com>
2026-06-04 15:20:03 -0600

    elimina

--- elimina.php
+++ elimina.php
@@ -1,5 +1,19 @@
 <?php
-
+    include 'config.php';
+    $mensaje = "";
+    $clase_mensaje = "";
+    if($_SERVER ["REQUEST_METHOD"] == 'POST' && isset($_POST['set_a_eliminar'])){
+        $id_set= $_POST['set_a_eliminar'];
+        $sql = "DELETE FROM sets WHERE set_num = 'id_sets'";
+        $query = mysqli_query($conexion, $sql);
+        if ($query){
+            $mensaje = "La eliminación fue correcta";
+            $clase_mensaje = "mensaje-exito";
+        } else{
+            $mensaje = "La eliminación NO fue correcta";
+            $clase_mensaje = "mensaje-error";
+        }
+    }
 ?>
 
 <!DOCTYPE html>
